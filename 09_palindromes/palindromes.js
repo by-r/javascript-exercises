4900e1879691137610bcd5629c538dcc2f0c8a28
Mohd Razzeq <rzq272@gmail.com>
2023-08-18 19:53:53 +0800

    Add working solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const newstr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+	return newstr.split("").reverse().join("") == newstr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+	if (count < 0) return "OOPS";
+	const fib = [0, 1];
+	for (let index = 1; index < count; index++) {
+		fib.push(fib[index] + fib[index - 1]);
+	}
+	return fib[count];
 };
 
 // Do not edit below this line
