a1ad4e9aea396235ecb23904c9fac49e84de94b0
Mohd Razzeq <rzq272@gmail.com>
2023-08-18 19:20:48 +0800

    Add working solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+	let total = 0;
+	if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+	if (min < 0 || max < 0) return "ERROR";
+	if (min > max) {
+		const temp = min;
+		min = max;
+		max = temp;
+	}
+	for (let i = min; i < max + 1; i++) {
+		total += i;
+	}
+	return total;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,52 @@
-const add = function() {
-	
+const add = function (...args) {
+	let add = 0;
+	for (let i = 0; i < args.length; i++) {
+		add += args[i];
+	}
+	return add;
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+	let diff = 0;
+	for (let i = 0; i < args.length; i++) {
+		if (args[i + 1]) {
+			diff = args[i] - args[i + 1];
+		}
+	}
+	return diff;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	let sum = 0;
+	array.forEach((items) => {
+		sum += items;
+	});
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (...args) {
+	let prod = 1;
+	for (let i = 0; i < args.length; i++) {
+		prod *= args[i];
+	}
+	return prod;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	if (n == 0) return 1;
+	return n * factorial(n - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
